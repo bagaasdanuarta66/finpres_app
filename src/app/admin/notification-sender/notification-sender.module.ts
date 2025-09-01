cf6f92ea09c05e067d0f562bb70a2a4c69410049
bagaasdanuarta66 <anggabagas1423@gmail.com>
2025-09-01 20:52:44 +0700

    mencoba perbaikan terakhir.

--- src/app/admin/notification-sender/notification-sender.module.ts
+++ src/app/admin/notification-sender/notification-sender.module.ts
@@ -1,19 +1,19 @@
+// src/app/admin/notification-sender/notification-sender.module.ts
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // <-- 1. IMPORT INI
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { NotificationSenderPageRoutingModule } from './notification-sender-routing.module';
-
 import { NotificationSenderPage } from './notification-sender.page';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule, // <-- 2. TAMBAHKAN INI
+    FormsModule,
     IonicModule,
-    NotificationSenderPageRoutingModule
+    NotificationSenderPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [NotificationSenderPage]
 })
